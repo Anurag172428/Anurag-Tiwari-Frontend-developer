b34ebb8d045330c95715b6bfc996d91c7e6c82c8
Anurag172428 <115621929+Anurag172428@users.noreply.github.com>
2022-10-12 02:36:20 -0700

    Add files via upload
    These are the files from the source folder. 
    
    React app to show the SpaceX data in the grid and also allow you to search the SpaceX data

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Card, Input } from 'semantic-ui-react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+export default function Posts() {
+    const [APIData, setAPIData] = useState([])
+    const [filteredResults, setFilteredResults] = useState([]);
+    const [searchInput, setSearchInput] = useState('');
+    useEffect(() => {
+        axios.get(`https://a5theory.com/spacex/read`)
+            .then((response) => {
+                setAPIData(response.data);
+            })
+    }, [])
+    const searchItems = (searchValue) => {
+        setSearchInput(searchValue)
+        if (searchInput !== '') {
+            const filteredData = APIData.filter((item) => {
+                return Object.values(item.status,item.original_launch,item.type).join('').toLowerCase().includes(searchInput.toLowerCase())
+            })
+            setFilteredResults(filteredData)
+        }
+        else{
+            setFilteredResults(APIData)
+        }
+    }
+
+    return (
+        <div style={{ padding: 20 }}>
+                    <div  class="spacexhead">
+                    <h1 class='banhead'>Spacex Capsule</h1>
+</div>
+<div class='container'><div class='row'>
+            <Input icon='search'
+                placeholder='Search Your Capsule...'
+                onChange={(e) => searchItems(e.target.value)}
+            /></div></div>
+            <Card.Group itemsPerRow={5} style={{ marginTop: 20 }}>
+                {searchInput.length > 1 ? (
+                    filteredResults.map((item) => {
+                        return (
+                          
+                            <Card classname='card'>
+                                <div class='container'><div class='row'>
+                            <div class='col'>
+                                <Card.Content>
+                                    <Card.Header style={{ fontSize: "20px", fontFamily: 'Montserrat'}}>capsule-serial:{item.capsule_serial}</Card.Header>
+                                    <Card.Description><strong>Capsule-Id:</strong>{item.capsule_id} </Card.Description>
+                                    <Card.Description><strong>Status:</strong>{item.status} </Card.Description>
+                                    <Card.Description><strong>Original-Launch:</strong>{item.original_launch} </Card.Description>
+                                    <Card.Description><strong>Original-Launch-Unix:</strong>{item.original_launch_unix} </Card.Description>
+                                    <Card.Description><strong>Landings:</strong>{item.landings} </Card.Description>
+                                    <Card.Description><strong>Type:</strong>{item.type} </Card.Description>
+                                    <Card.Description><strong>Details:</strong>{item.details} </Card.Description>
+                                    <Card.Description><strong>Reuse-Count:</strong>{item.reuse_count} </Card.Description>
+
+                                                                    </Card.Content>
+                                                                    </div></div></div>
+                                
+                            </Card>
+                        )
+                    })
+                ) : (
+                    APIData.map((item, _capsule_serial) => {
+
+                        return (                     
+   
+                            <Card classname='card' key={item._capsule_serial}><div class='container'><div class='row'>
+                            <div class='col'>
+                                    
+                                <Card.Content>
+                                    <Card.Header style={{ fontSize: "20px", fontFamily: 'Montserrat'}}>capsule-serial:{item.capsule_serial}</Card.Header>
+                                    <Card.Description><strong>Capsule-Id:</strong>{item.capsule_id} </Card.Description>
+                                    <Card.Description><strong>Status:</strong>{item.status} </Card.Description>
+                                    <Card.Description><strong>Original-Launch:</strong>{item.original_launch} </Card.Description>
+                                    <Card.Description><strong>Original-Launch-Unix:</strong>{item.original_launch_unix} </Card.Description>
+                                    <Card.Description><strong>Landings:</strong>{item.landings} </Card.Description>
+                                    <Card.Description><strong>Type:</strong>{item.type} </Card.Description>
+                                    <Card.Description><strong>Details:</strong>{item.details} </Card.Description>
+                                    <Card.Description><strong>Reuse-Count:</strong>{item.reuse_count} </Card.Description>
+                                                                    </Card.Content>
+                                
+                                                                    </div></div></div>
+                            </Card>
+                            
+                            
+                            
+                        )
+                    })
+                )}
+            </Card.Group>
+        </div>
+    )
+    
+}
---
+++ src/fetchdata.js
@@ -0,0 +1,1 @@
+log.cosole('hello');
